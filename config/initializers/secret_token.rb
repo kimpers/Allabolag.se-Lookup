2de31dd97ff857757d384c6478f2aa1618a891b2
Kim <kimpersson88@gmail.com>
2014-02-19 01:07:31 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AllaBolag::Application.config.secret_key_base = '55d97cdf4fd9e81011ac6b46fb62b277b5d94c135004abbd3b5d9982b7ba949759c205080390498c664014084c172584dc797f8ee0ccea46501a297a83eda7fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AllaBolag::Application.config.session_store :cookie_store, key: '_AllaBolag_session'
