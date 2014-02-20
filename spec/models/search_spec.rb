1074797e35347122d244ae795a75995bef9709b5
Kim <kimpersson88@gmail.com>
2014-02-20 01:59:54 +0100

    fixed testing

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Search do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attributes = {:query => "Company AB", :result => "CMP"}
+  end
+  it 'should create a new search' do
+    Search.create!(@attributes)
+  end
+
+  it 'should find already created search' do
+    Search.create(@attributes)
+    Search.find_by_query(@attributes[:query]).should be_valid
+  end
+
+  it 'should not find non existing search' do
+    Search.find_by_query("Fake company").should be_an_blank
+end
 end
